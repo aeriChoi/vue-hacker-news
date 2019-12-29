ffd2b2bc5c56eea7c0ec886a027da6a480093bdf
aeri choi <aeri.choi715@gmail.com>
2019-12-30 00:02:15 +0900

    Add user view markup

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="user">
     <div class="user-info-card">
-      <p>{{ getUserInfo.id }}</p>
-      <p>{{ getUserInfo.created }}</p>
-      <p>{{ getUserInfo.karma }}</p>
+      <p class="card-title">
+        <strong>User Info</strong>
+      </p>
+      <p class="card-des">
+        <strong>User Name:</strong>
+        <span>{{ getUserInfo.id }}</span>
+      </p>
+      <p class="card-des">
+        <strong>Created:</strong>
+        <span>{{ getUserInfo.created }}</span>
+      </p>
+      <p class="card-des">
+        <strong>Karma:</strong>
+        <span>{{ getUserInfo.karma }}</span>
+      </p>
     </div>
   </div>
 </template>
@@ -26,5 +38,29 @@ export default {
 </script>
 
 <style lang="scss">
+.user-info-card {
+  width: 85%;
+  padding: 1.3rem 2rem;
+  margin: 3rem auto;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  color: #000;
+  font-size: 1.3rem;
 
+  p {
+
+    &.card-title {
+      margin-bottom: 2rem;
+      font-size: 2rem;
+    }
+
+    &.card-des {
+      margin-bottom: .6rem;
+
+      strong {
+        margin-right: 1rem;
+      }
+    }
+  }
+}
 </style>
