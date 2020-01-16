351c6415091bb199481323a6d927ef39da095fda
aeriChoi <aeri.choi715@gmail.com>
2020-01-16 15:20:41 +0900

    Update ListItem

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,9 +1,74 @@
 <template>
-  <div></div>
+  <div class="list-sec">
+    <ul class="list-box">
+      <li class="list-item" v-for="item in getNewsList" v-bind:key="item.id">
+        <div class="points">
+          {{ item.points || 0 }}
+        </div>
+        <div class="item-info-box">
+          <p v-if="item.domain !== null" class="domain-info-box">
+            <span class="des-icon">
+              <font-awesome-icon icon="home" />
+            </span>
+            <a class="domain-des-url" :href="item.url" target="_blank">
+              {{ item.domain }}
+            </a>
+          </p>
+          <h2 class="script-title">
+            <strong>
+              <a v-bind:href="item.url" target="_blank">
+                {{ item.title }}
+              </a>
+            </strong>
+          </h2>
+          <div class="des-info-box">
+            <ul class="des-info-list">
+              <li class="des-info-list-item">
+                <span class="des-icon">
+                  <font-awesome-icon :icon="['far', 'user']" />
+                </span>
+                <span class="des-text user-name">
+                  <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+                </span>
+              </li>
+              <li class="des-info-list-item">
+                <span class="des-icon">
+                  <font-awesome-icon :icon="['far', 'clock']" />
+                </span>
+                <span class="des-text time-ago">
+                  {{ item.time_ago }}
+                </span>
+              </li>
+              <li class="des-info-list-item">
+                <span class="des-icon">
+                  <font-awesome-icon :icon="['far', 'comment']" />
+                </span>
+                <span class="des-text comments">
+                  {{ item.comments_count }}
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import '../assets/style/list-item.scss'
+
 export default {
+  computed: {
+    ...mapGetters([
+      'getNewsList'
+    ])
+  },
+  created () {
+    this.$store.dispatch('GET_NEWS')
+  }
 }
 </script>
 
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,81 +1,18 @@
 <template>
   <section id="news" class="list-sec">
-    <ul class="list-box">
-      <li class="list-item" v-for="item in getNewsList" v-bind:key="item.id">
-        <div class="points">
-          {{ item.points || 0 }}
-        </div>
-        <div class="item-info-box">
-          <p class="domain-info-box">
-            <span class="des-icon">
-              <font-awesome-icon icon="home" />
-            </span>
-            <a class="domain-des-url" :href="item.url" target="_blank">
-              {{ item.domain }}
-            </a>
-          </p>
-          <h2 class="script-title">
-            <strong>
-              <a v-bind:href="item.url" target="_blank">
-                {{ item.title }}
-              </a>
-            </strong>
-          </h2>
-          <div class="des-info-box">
-            <ul class="des-info-list">
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'user']" />
-                </span>
-                <span class="des-text user-name">
-                  <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
-                </span>
-              </li>
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'clock']" />
-                </span>
-                <span class="des-text time-ago">
-                  {{ item.time_ago }}
-                </span>
-              </li>
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'comment']" />
-                </span>
-                <span class="des-text comments">
-                  {{ item.comments_count }}
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </li>
-    </ul>
+    <ListItem></ListItem>
   </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-// import ListItem from '../components/ListItem.vue'
-
-import '../assets/style/list-item.scss'
+import ListItem from '../components/ListItem.vue'
 
 export default {
-  // components: {
-  //   ListItem
-  // }
-  computed: {
-    ...mapGetters([
-      'getNewsList'
-    ])
-  },
-  created () {
-    this.$store.dispatch('GET_NEWS')
+  components: {
+    ListItem
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
