293da73ec45fd0ee216cea3a5c6a8bb8cded5c9a
aeriChoi <aeri.choi715@gmail.com>
2020-01-16 16:31:44 +0900

    Update ListItem

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,67 +1,16 @@
 <template>
   <section id="ask">
     <ListItem></ListItem>
-    <!-- <ul class="list-box">
-      <li class="list-item" v-for="item in getAskList" v-bind:key="item.id">
-        <div class="points">
-          {{ item.points || 0 }}
-        </div>
-        <div class="item-info-box">
-          <h2 class="script-title">
-            <strong>
-              <router-link v-bind:to="`item/${item.id}`">{{ item.title }}</router-link>
-            </strong>
-          </h2>
-          <div class="des-info-box">
-            <ul class="des-info-list">
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'user']" />
-                </span>
-                <span class="des-text user-name">
-                  <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
-                </span>
-              </li>
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'clock']" />
-                </span>
-                <span class="des-text time-ago">
-                  {{ item.time_ago }}
-                </span>
-              </li>
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'comment']" />
-                </span>
-                <span class="des-text comments">
-                  {{ item.comments_count }}
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </li>
-    </ul> -->
   </section>
 </template>
 
 <script>
-// import { mapGetters } from 'vuex'
 import ListItem from '../components/ListItem.vue'
 
 export default {
   components: {
     ListItem
   }
-  // computed: {
-  //   ...mapGetters([
-  //     'getAskList'
-  //   ])
-  // },
-  // created () {
-  //   this.$store.dispatch('GET_ASK')
-  // }
 }
 </script>
 
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,57 +1,15 @@
 <template>
-  <section id="jobs" class="list-sec">
-    <ul class="list-box">
-      <li class="list-item" v-for="job in getJobsList" v-bind:key="job.id">
-        <div class="points">
-          {{ job.points || 0 }}
-        </div>
-        <div class="item-info-box">
-          <p class="domain-info-box">
-            <span class="des-icon">
-              <font-awesome-icon icon="home" />
-            </span>
-            <a class="domain-des-url" :href="job.url" target="_blank">
-              {{ job.domain }}
-            </a>
-          </p>
-          <h2 class="script-title">
-            <strong>
-              <a v-bind:href="job.url" target="_blank">
-                {{ job.title }}
-              </a>
-            </strong>
-          </h2>
-          <div class="des-info-box">
-            <ul class="des-info-list">
-              <li class="des-info-list-item">
-                <span class="des-icon">
-                  <font-awesome-icon :icon="['far', 'clock']" />
-                </span>
-                <span class="des-text time-ago">
-                  {{ job.time_ago }}
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </li>
-    </ul>
+  <section id="jobs">
+    <ListItem></ListItem>
   </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
-import '../assets/style/list-item.scss'
+import ListItem from '../components/ListItem.vue'
 
 export default {
-  computed: {
-    ...mapGetters([
-      'getJobsList'
-    ])
-  },
-  created () {
-    this.$store.dispatch('GET_JOBS')
+  components: {
+    ListItem
   }
 }
 </script>
